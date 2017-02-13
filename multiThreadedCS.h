0b67a720c3950a966d2fb3b4475b41ddc8050cdc
asanche <asanche@ualberta.ca>
2017-02-13 02:03:23 +0000

    move sample code to sample code folder

---
+++ multiThreadedCS.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+
+#define MAX_STRING_LENGTH 1024
+#define ARRAY_SIZE 1024
